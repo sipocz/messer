3abd5799ef7e2077a08867519ee4fc6a235c9a51
László Sipőcz <sipoczlaszlo@gmail.com>
2021-04-13 00:21:44 +0200

    Update visual_view.py

--- visual_view.py
+++ visual_view.py
@@ -1,7 +1,4 @@
-def printX(a):
-  print(a)
-  
-def grafikon(fx,desc1,txt1,desc2="",txt2="",ngraf=2,c1='rgba(0,200,0,0.8)', c2='rgba(200,0,0,0.3)',title=none):
+def grafikon(fx,desc1,txt1,desc2="",txt2="",ngraf=2,c1='rgba(35,128,132,0.8)', c2='rgba(200,0,0,0.3)',title=None):
     '''
     fx: dataFrame
     desc1:column1
@@ -11,19 +8,18 @@ def grafikon(fx,desc1,txt1,desc2="",txt2="",ngraf=2,c1='rgba(0,200,0,0.8)', c2='
     ngraf: number of graph
     c2: color1
     c2: color2
-
     '''
     
     #x_=[i for i in range(len(y_pred))]
-    if title==none:
+    if title==None:
       title=txt1+txt2
     import plotly.express as px
     import plotly.graph_objects as go
     from plotly.subplots import make_subplots
-    fig0 = make_subplots(rows=1, cols=1,title=title)
+    fig0 = make_subplots(rows=1, cols=1)
     
     fig0.add_trace(
-        go.Scatter(x=fx.index, y=fx[desc1], name=txt1,line=dict(color=c1) ,showlegend=True  ),
+        go.Scatter(x=fx.index, y=fx[desc1], name=txt1, line=dict(color=c1) ,showlegend=True  ),
 
         row=1, col=1
 
@@ -37,9 +33,11 @@ def grafikon(fx,desc1,txt1,desc2="",txt2="",ngraf=2,c1='rgba(0,200,0,0.8)', c2='
         )
     
     fig0.update_layout(
+        title=title,
         autosize=False,
         width=1200,
         height=600,
+        
         )
 
     fig0.show()
